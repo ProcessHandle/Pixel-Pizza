015813e9ab73270cd2e5950f3ffbeb4c3fb9ac35
Mbrenneman0 <mbrenneman0@gmail.com>
2025-12-09 10:25:31 -0500

    fixed some login stuff

--- script/login.js
+++ script/login.js
@@ -372,6 +372,7 @@ function handleLogInOut() {
         uiUpdate(false);
         updCartTotal();
         statusMessage("Logged out successfully", 2000);
+        navigate(navLinks.home);
     } else {
         login();
     }
@@ -478,11 +479,15 @@ function toggleRegister() {
         console.log("show login, hide register");
         loginContainer.classList.remove("hidden");
         registerContainer.classList.add("hidden");
+        document.getElementById("register-link").innerHTML = `<a onclick="toggleRegister()">New Player? <span>Create Account.</span></a>`
+        document.getElementById("login-out-btn").innerText = "Login"
     }
     else {
         console.log("hide login, show register")
         loginContainer.classList.add("hidden");
         registerContainer.classList.remove("hidden");
+        document.getElementById("register-link").innerHTML = `<a onclick="toggleRegister()">Returning Player? <span>Log In.</span></a>`
+        document.getElementById("login-out-btn").innerText = "Register"
     }
     animateHeight();
 }
